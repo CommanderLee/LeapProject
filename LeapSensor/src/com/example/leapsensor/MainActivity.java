56a9a0e2fc7bd7e6d0e72376bce376644c0e3b9f
LiZhen <dao.yi.er.san@gmail.com>
2014-05-02 15:29:59 +0800

    Complete Handler and Runnable.
    Frame of the App. TODO: the sensor data (x,y,z)

--- LeapSensor/src/com/example/leapsensor/MainActivity.java
+++ LeapSensor/src/com/example/leapsensor/MainActivity.java
@@ -9,7 +9,9 @@ import java.util.Calendar;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
 import android.os.StatFs;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -25,12 +27,16 @@ import android.widget.Toast;
  */
 public class MainActivity extends Activity {
 
-	File			sdCardDir = null;
 	TextView		tvSDMem;
 	EditText		etTimeInterval;
-	Button			buttonStart;
+	Button			buttonStart, buttonEnd;
 	
+	File			sdCardDir = null;
+	FileWriter		fWriter = null;
+	long			startTime = 0;
 	double			timeInterval;
+	Handler			sensorHandler = null;
+	Runnable		runnable;
 	
 	/** Set the range of the valid time interval */
 	final double	MIN_TIME = 1.0;
@@ -55,7 +61,27 @@ public class MainActivity extends Activity {
         return true;
     }
     
-    /**
+    
+    
+    @Override
+	protected void onDestroy() {
+		super.onDestroy();
+		if (fWriter != null)
+		{
+			try {
+				fWriter.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		if (sensorHandler != null)
+		{
+			sensorHandler.removeCallbacks(runnable);
+		}
+	}
+
+
+	/**
      * Get Empty SD Memory Size and Display.
      */
     private void detectSDCard()
@@ -75,13 +101,12 @@ public class MainActivity extends Activity {
         else
         {
         	/** SD Card not available */
-        	Toast errSD = Toast.makeText(getApplicationContext(), "SD Card Not Found", Toast.LENGTH_SHORT);
-        	errSD.show();
+        	Toast.makeText(getApplicationContext(), "SD Card Not Found", Toast.LENGTH_SHORT).show();
         }
     }
     
     /**
-     * Set Listener of the Start Button.
+     * Set Listener of the Start / End Button.
      * Check the range of the Time Interval.
      */
     private void setButtonListener()
@@ -95,23 +120,44 @@ public class MainActivity extends Activity {
 				timeInterval = Double.valueOf(etTimeInterval.getText().toString());
 				if (timeInterval < MIN_TIME)
 				{
-					Toast minTime = Toast.makeText(getApplicationContext(), "Time Interval too small", Toast.LENGTH_SHORT);
-					minTime.show();
+					Toast.makeText(getApplicationContext(), "Time Interval too small", Toast.LENGTH_SHORT).show();
 					etTimeInterval.setText(String.valueOf(DEFAULT_TIME));
 				}
 				else if (timeInterval > MAX_TIME)
 				{
-					Toast maxTime = Toast.makeText(getApplicationContext(), "Time Interval too big", Toast.LENGTH_SHORT);
-					maxTime.show();
+					Toast.makeText(getApplicationContext(), "Time Interval too big", Toast.LENGTH_SHORT).show();
 					etTimeInterval.setText(String.valueOf(DEFAULT_TIME));
 				}
 				else
 				{
 					/** Start the real work */
-					Toast start = Toast.makeText(getApplicationContext(), "Start!", Toast.LENGTH_SHORT);
-					start.show();
+					Toast.makeText(getApplicationContext(), "Start!", Toast.LENGTH_SHORT).show();
 					
 					recordSensor();
+				}
+			}
+		});
+    	
+    	buttonEnd = (Button) findViewById(R.id.buttonEnd);
+    	buttonEnd.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				if (fWriter == null || sensorHandler == null)
+				{
+					Toast.makeText(getApplicationContext(), "Not Started", Toast.LENGTH_SHORT).show();
+				}
+				else
+				{
+					/** Close the file writer and handler */
+					try {
+						fWriter.close();
+						sensorHandler.removeCallbacks(runnable);
+						Toast.makeText(getApplicationContext(), "Write Succeed", Toast.LENGTH_SHORT).show();
+					} catch (IOException e) {
+						Toast.makeText(getApplicationContext(), "IOException", Toast.LENGTH_SHORT).show();
+					}
+					
 				}
 			}
 		});
@@ -125,11 +171,11 @@ public class MainActivity extends Activity {
     	if (sdCardDir == null)
     	{
     		/** SD Card not available */
-        	Toast errSD = Toast.makeText(getApplicationContext(), "SD Card Not Found", Toast.LENGTH_SHORT);
-        	errSD.show();
+        	Toast.makeText(getApplicationContext(), "SD Card Not Found", Toast.LENGTH_SHORT).show();
         	return;
     	}
     	
+    	/** Store the data in '/sdcard0/LeapData/[LocalCalendarTime].csv' */
     	String path = sdCardDir + "/LeapData";
     	String name = "/" + Calendar.getInstance().getTime().toString() + ".csv";
     	
@@ -143,14 +189,37 @@ public class MainActivity extends Activity {
     	
     	try {
     		fName.createNewFile();
-    		FileWriter fw = new FileWriter(fName);
+    		fWriter = new FileWriter(fName);
+    		
+    		/** Write for each timeInterval ms */
+    		sensorHandler = new Handler();
+    		runnable = new Runnable() {
+				
+				@Override
+				public void run() {
+					/** Get data from the acceleration sensors */
+					try {
+						fWriter.write(String.valueOf(Calendar.getInstance().getTimeInMillis() - startTime) + ",");
+						fWriter.write("\r\n");
+					} catch (IOException e) {
+						Log.d("IO", "Write Failed");
+						e.printStackTrace();
+					}
+					
+					sensorHandler.postDelayed(this, (long)timeInterval);
+				}
+			};
+			startTime = Calendar.getInstance().getTimeInMillis();
+    		sensorHandler.postDelayed(runnable, (long)timeInterval);
+			
+    		/*FileWriter fw = new FileWriter(fName);
     		fw.write("1.0, 1, 2, 3");
     		fw.write("\r\n");
     		fw.write("2.0, 4, 5, 6");
     		fw.write("\r\n");
     		fw.close();
-
     		Toast.makeText(getApplicationContext(), "Write Succeed", Toast.LENGTH_SHORT).show();
+    		*/
     	} catch (FileNotFoundException e) {
     		Toast.makeText(getApplicationContext(), "File Not Found", Toast.LENGTH_SHORT).show();
     	} catch (IOException e) {
